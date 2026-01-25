0fd7a8e1566d9b8a8378fce60685f5d658586907
AmalizzyDHY <amalizzy.dhy@gmail.com>
2026-01-25 17:39:42 +0100

    Ajout de la base de donnée, et création du lien entre le python et la base et création d'une array avec l'ensemble de la base de donner, et choix du pokemon aléatoirement

--- Fonc_Lisa/traitement_bd.py
+++ Fonc_Lisa/traitement_bd.py
@@ -1,6 +1,25 @@
 import os
-import pandas as pd
+import pandas
+import csv
+import random
+
+print("Dossier courant :", os.getcwd())
+print("Contenu du dossier :", os.listdir())
+
 
 print("poulpe")
+pokedex = []
 
-pokedex = pd.read_csv("pokemon_data.csv")
+with open("Fonc_Lisa/pokemon_data.csv", newline='', encoding="utf-8") as fichier:
+    test = csv.reader(fichier, delimiter=",")
+    for ligne in test:
+        #print(ligne)
+        #print("\n")
+        pokedex.append(ligne)
+    
+    # print(type(pokedex))
+    # print(pokedex[0])
+    # print(pokedex[1])
+
+n_pkmn_mystere = random.randint(1, 1025)
+print(pokedex[n_pkmn_mystere])
