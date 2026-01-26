20e6a98d72d8503d6957560a3d2157bce9fc0f3e
AmalizzyDHY <amalizzy.dhy@gmail.com>
2026-01-26 09:25:36 +0100

    test pour voir si ça fonctionne et si ça peut fonctionner

--- Fonc_Lisa/traitement_bd.py
+++ Fonc_Lisa/traitement_bd.py
@@ -3,23 +3,35 @@ import pandas
 import csv
 import random
 
-print("Dossier courant :", os.getcwd())
-print("Contenu du dossier :", os.listdir())
+# print("Dossier courant :", os.getcwd()) # Pour savoir où on est
+# print("Contenu du dossier :", os.listdir()) # Pour savoir ce qu'il y a avec nous
 
-
-print("poulpe")
-pokedex = []
+# print("poulpe")
+pokedex = [] # array
+win = 0
 
 with open("Fonc_Lisa/pokemon_data.csv", newline='', encoding="utf-8") as fichier:
     test = csv.reader(fichier, delimiter=",")
     for ligne in test:
         #print(ligne)
         #print("\n")
-        pokedex.append(ligne)
+        pokedex.append(ligne) # On ouvre le fichier csv et on ajout manuellement toutes les lignes dans l'array
     
     # print(type(pokedex))
-    # print(pokedex[0])
-    # print(pokedex[1])
+    print(pokedex[0]) # On affiche pour être sûr
+    # print(pokedex[58])
 
-n_pkmn_mystere = random.randint(1, 1025)
+# choix du pokemon "mystère"
+n_pkmn_mystere = random.randint(1, 1025) # On choisis un numéro entre 1 et 1025, ce qui choisis le numéro du pokemon "mystère"
 print(pokedex[n_pkmn_mystere])
+
+while not win:
+    # L'utilisateur choisis un pokemon, pour deviner le pokemon mystère
+    n_pkmn_guest = int(input("Try : "))
+    print(pokedex[n_pkmn_guest])
+
+    if n_pkmn_guest == n_pkmn_mystere :
+        print("Tu as gagner ! C'était bien " + str(pokedex[n_pkmn_mystere][1]))
+        win = 1
+    else :
+        print("Essaie encore")
